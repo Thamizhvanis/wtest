9dd3e7674327ac697c459a12baf7b169b08de53f
Thamizhvani <Thamizhvani@Thamizh>
2023-04-14 19:35:51 +0530

    c4

--- wtest/src/test/java/wtest/wtestrun.java
+++ wtest/src/test/java/wtest/wtestrun.java
@@ -1,26 +1,25 @@
 package wtest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class wtestrun {
-
-	public static void main(String[] args) throws Exception {
-		
-		Browser1();
-
-	}
 	
 	@Test
-	public static void Browser1() throws InterruptedException {
+	public static void Browser1() throws InterruptedException, MalformedURLException {
 		
-		WebDriver driver;
+		//WebDriver driver;
+		RemoteWebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		//System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
@@ -32,10 +31,11 @@ public class wtestrun {
 		//options.addArguments("disable-infobars");
 		//options.addArguments("disable-gpu");
 		options.merge(caps);
-		//URL url = new URL("http://3.95.161.130:49154/wd/hub");
-		driver = new ChromeDriver(options);
+		URL url = new URL("http://18.233.10.79:49154/wd/hub");
+		//driver = new ChromeDriver(options);
+		driver = new RemoteWebDriver(url, options);
 		//driver.get("http://3.95.161.130:49154/wd/hub");
-		driver.get("http://webapp");
+		driver.get("http://18.233.10.79");
 		//driver.get("http://18.233.10.79");
 		Thread.sleep(6000);
 		System.out.print("Title of the page is: " + driver.getTitle() + "\n");
