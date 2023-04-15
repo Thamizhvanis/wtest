7dfafb0d2194e46d9b6c7447c9b5db98ff19ddbe
Thamizhvani <Thamizhvani@Thamizh>
2023-04-15 23:14:29 +0530

    c2

--- wtest/src/test/java/wtest/wtestmain.java
+++ wtest/src/test/java/wtest/wtestmain.java
@@ -2,13 +2,13 @@ package wtest;
 
 import org.testng.TestNG;
 
+
 public class wtestmain {
 
 	public static void main(String[] args) {
 		TestNG testNg = new TestNG();
-		 testNg.setTestClasses(new Class[] {wtestrun.class});
+		 testNg.setTestClasses(new Class[] {wtest.class});
 			testNg.run();
-
 
 	}
 
